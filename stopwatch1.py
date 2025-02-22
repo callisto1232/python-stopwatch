0fdd85ec4bb2db1b2ab57b26beffe79ad312a257
callisto1232 <146484672+callisto1232@users.noreply.github.com>
2025-02-22 15:45:26 +0300

    Update stopwatch1.py

--- stopwatch1.py
+++ stopwatch1.py
@@ -3,13 +3,8 @@ start = datetime.now()
 seconds = set()
 while 1:
     current = datetime.now()
-    elapsed = (current - start).total_seconds()
-    hour = int(elapsed // 3600)
-    minute = int((elapsed % 3600) // 60)
-    second = int(elapsed % 60)
-    if second not in seconds:
-        seconds.add(second)
-        print(f"{round(elapsed)} \t {current}")
-    if second == 59:
-        seconds = set()
+    hour = current.hour - start.hour
+    minute = current.minute - start.minute
+    second = current.second - start.second
+    print(hour, minute, second)
 
